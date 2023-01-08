49ab2aac36810398394ccfe2f264cb06d2ee5c08
silicom-sts <115641377+silicom-sts@users.noreply.github.com>
2023-01-08 10:55:57 +0200

    Update suite_test.go

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	stsv1alpha1 "github.com/rmr/sts-operator/api/v1alpha1"
+	stsv1alpha1 "github.com/silicom-sts/sts-operator/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
 )
 
